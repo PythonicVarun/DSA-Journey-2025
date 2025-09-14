d726228ca884ee6d230099562afa0405c87460ab
Varun Agnihotri <code@pythonicvarun.me>
2025-09-14 13:28:49 +0530

    Day 7: 3 problems solved

---
+++ Week-01/Day-07/largest-prime-factor.py
@@ -0,0 +1,19 @@
+"""
+Problem: Largest prime factor
+Date: 14-09-2024
+Difficulty: Medium
+Time taken: ~2 minutes
+Link: https://www.geeksforgeeks.org/problems/largest-prime-factor2601/1
+"""
+
+
+class Solution:
+    def largestPrimeFactor(self, n):
+        # code here
+        factor = 2
+        while factor * factor <= n:
+            if n % factor == 0:
+                n //= factor
+            else:
+                factor += 1
+        return n
---
+++ Week-01/Day-07/vowel-spellchecker.py
@@ -0,0 +1,35 @@
+"""
+Problem: Vowel Spellchecker
+Date: 14-09-2024
+Difficulty: Medium
+Time taken: ~5 minutes
+Link: https://leetcode.com/problems/vowel-spellchecker/
+"""
+
+import re
+from typing import List
+
+
+class Solution:
+    def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        res = []
+        mask = lambda s: re.sub(r"[aeiou]", "a", s.lower())
+
+        lst = set(wordlist)
+        lowerlst = {}
+        maskedlst = {}
+        for w in wordlist[::-1]:
+            lowerlst[w.lower()] = w
+            maskedlst[mask(w)] = w
+
+        for q in queries:
+            if q in lst:
+                res.append(q)
+                continue
+
+            if q.lower() in lowerlst:
+                res.append(lowerlst[q.lower()])
+                continue
+
+            res.append(maskedlst.get(mask(q), ""))
+        return res
