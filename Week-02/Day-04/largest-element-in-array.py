72acd27eb85caf79501bde22cc2149a3a092c0c7
Varun Agnihotri <code@pythonicvarun.me>
2025-09-18 18:16:06 +0530

    Day 11: 3 problems solved

---
+++ Week-02/Day-04/largest-element-in-array.py
@@ -0,0 +1,13 @@
+"""
+Problem: Largest Element in Array
+Date: 18-09-2024
+Difficulty: Easy
+Time taken: ~30 seconds
+Link: https://geeksforgeeks.org/problems/largest-element-in-array4009/1
+"""
+
+
+class Solution:
+    def largest(self, arr):
+        # code here
+        return max(arr)
---
+++ Week-02/Day-04/maximum-subarray.py
@@ -0,0 +1,19 @@
+"""
+Problem: Maximum Subarray
+Date: 18-09-2024
+Difficulty: Medium
+Time taken: ~1 minute
+Link: https://leetcode.com/problems/maximum-subarray/
+"""
+
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
+        currSum = nums[0]
+        for i in nums[1:]:
+            currSum = max(i + currSum, i)
+            res = max(res, currSum)
+        return res
