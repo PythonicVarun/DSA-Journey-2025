d442350ee9d96ad0bd7197b43d6e866fc7f72055
Varun Agnihotri <code@pythonicvarun.me>
2025-09-29 09:05:28 +0530

    Day 22: 1 problem solved

---
+++ Week-04/Day-01/minimum-score-triangulation-of-polygon.py
@@ -0,0 +1,28 @@
+"""
+Problem: Minimum Score Triangulation of Polygon
+Date: 29-09-2025
+Difficulty: Medium
+Time taken: ~20 minutes
+Link: https://leetcode.com/problems/minimum-score-triangulation-of-polygon
+"""
+
+from functools import lru_cache
+from typing import List
+
+
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        @lru_cache(None)
+        def dp(i, j):
+            if i + 2 > j:
+                return 0
+
+            if i + 2 == j:
+                return values[i] * values[i + 1] * values[j]
+
+            return min(
+                (values[i] * values[k] * values[j] + dp(i, k) + dp(k, j))
+                for k in range(i + 1, j)
+            )
+
+        return dp(0, len(values) - 1)
